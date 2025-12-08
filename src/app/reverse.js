1edc9f458797801921957fab45c01dfc97b588eb
Gowthami Nimanagoti <gowthami.nimanagoti@visaka.in>
2025-12-08 17:17:01 +0530

    reverse

--- src/app/reverse.js
+++ src/app/reverse.js
@@ -1,31 +1,10 @@
-/*const express = require("express");
-const app = express();
-app.get("/reverse", (req, res) => {
-    const name = req.query.name || "Gowthami Nimanagoti";
-
-    const reversed = name.split(" ").map(word => word.split("")).reverse().join("").toLowercase().join(" ");
-    res.send({
-        original: name,
-        reversed: reversed
-    }); 
-});
-app.listen(5000, () => console.log('Server running on http://localhost:5000'));*/
-
-
-
-
+//reverse
 const express = require("express");
 const app = express();
 
 app.get("/reverse", (req, res) => {
     const name = req.query.name || "Gowthami Nimanagoti";
-
-    // Reverse EACH WORD separately
-    const reversed = name
-        .split(" ")                      // ["Gowthami", "Nimanagoti"]
-        .map(word => word.split("").reverse().join("")) // ["imah twoG", ...]
-        .join(" ");                      // join back with space
-
+    const reversed = name.split(" ").map(word => word.split("").reverse().join("")).join(" ");                     
     res.send({
         original: name,
         reversed: reversed
